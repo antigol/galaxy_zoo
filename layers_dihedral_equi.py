a91c2b8ec18f80c2ee890ef98902345f0b5fbc4c
Mario Geiger <geiger.mario@gmail.com>
2017-03-02 13:47:54 +0100

    arch_inv_c

--- layers_dihedral_equi.py
+++ layers_dihedral_equi.py
@@ -20,8 +20,10 @@ def scaleandshift(x, a0=1, b0=0, input_repr='regular'):
     f = x.get_shape().as_list()[-1]
     with tf.name_scope("scaleandshift"):
         if input_repr == 'invariant':
-            a = tf.Variable(tf.constant(a0, dtype=tf.float32, shape=[f]), name="g")
-            b = tf.Variable(tf.constant(b0, dtype=tf.float32, shape=[f]), name="b")
+            a = tf.Variable(tf.constant(
+                a0, dtype=tf.float32, shape=[f]), name="g")
+            b = tf.Variable(tf.constant(
+                b0, dtype=tf.float32, shape=[f]), name="b")
         if input_repr == 'regular':
             assert f % 8 == 0
             a = tf.tile(tf.Variable(tf.constant(
@@ -33,17 +35,64 @@ def scaleandshift(x, a0=1, b0=0, input_repr='regular'):
         return a * x + b
 
 
-def fullyconnected(x, f_out=None, output_repr='regular', activation=relu, name='fc'):
+# pylint: disable=R0913
+def fullyconnected2(x_inv, x_reg, f_inv=None, f_reg=None,
+                    activation=relu, name='fullyconnected2'):
+    """take two tensors and output two tensors"""
+    if f_inv is None:
+        f_inv = x_inv.get_shape().as_list()[1]
+    if f_reg is None:
+        f_reg = x_reg.get_shape().as_list()[1] // 8
+
+    with tf.name_scope(name):
+        inv = fc(x_inv, f_inv, 'invariant', 'invariant')
+        reg = fc(x_inv, 8*f_reg, 'invariant', 'regular')
+
+        if x_reg is not None:
+            inv += fc(x_reg, f_inv, 'regular', 'invariant')
+            reg += fc(x_reg, 8*f_reg, 'regular', 'regular')
+            inv *= 1 / math.sqrt(2)
+            reg *= 1 / math.sqrt(2)
+
+        b = tf.Variable(tf.constant(0.0, shape=[f_inv]), name="b")
+        tf.summary.histogram("bias_inv", b)
+        inv += b
+
+        b = tf.Variable(tf.constant(0.0, shape=[f_reg]), name="b")
+        tf.summary.histogram("bias_reg", b)
+        reg += tf.tile(b, [8])
+
+        if activation:
+            inv = activation(inv)
+            reg = activation(reg)
+
+        return (inv, reg)
+
+def fullyconnected(x, f_out=None, input_repr='reguar', output_repr='regular',
+                   activation=relu, name='fullyconnected'):
+    """..."""
+    with tf.name_scope(name):
+        x = fc(x, f_out, input_repr, output_repr)
+
+        if output_repr == 'invariant':
+            x += tf.Variable(tf.constant(0.0, shape=[f_out]), name="b")
+
+        if output_repr == 'regular':
+            x += tf.tile(tf.Variable(tf.constant(0.0, shape=[f_out // 8]), name="b"), [8])
+
+        return activation(x) if activation else x
+
+def fc(x, f_out=None, input_repr='reguar', output_repr='regular', name='fc'):
     """Fully connect a tensor which transforms with the regular representation
     with an output tensor which transforms either with the regular or invariant representation"""
     assert output_repr == 'regular' or output_repr == 'invariant'
 
     f_in = x.get_shape().as_list()[1]
-    assert f_in % 8 == 0
-    if f_out is None:
-        f_out = f_in
 
-    if output_repr == 'regular':
+    if input_repr == 'regular' and output_repr == 'regular':
+        assert f_in % 8 == 0
+        if f_out is None:
+            f_out = f_in
         assert f_out % 8 == 0
 
         with tf.name_scope("{}-8x{}-8x{}".format(name, f_in // 8, f_out // 8)):
@@ -69,37 +118,97 @@ def fullyconnected(x, f_out=None, output_repr='regular', activation=relu, name='
                     for i in range(8)], 0)
                 for j in range(8)], 1)
 
-            x = tf.matmul(x, W)
+            return tf.matmul(x, W)
 
-            b = tf.Variable(tf.constant(0.0, shape=[f_out // 8]), name="b")
-            tf.summary.histogram("bias", b)
-            x = x + tf.tile(b, [8])
+    if input_repr == 'regular' and output_repr == 'invariant':
+        assert f_in % 8 == 0
+        if f_out is None:
+            f_out = f_in // 8
 
-            if activation is not None:
-                x = activation(x)
-            return x
-
-    if output_repr == 'invariant':
         with tf.name_scope("{}-8x{}-{}".format(name, f_in // 8, f_out)):
             W0 = tf.random_normal([f_in // 8, f_out])
             W0 = W0 / math.sqrt(f_in)
             W = tf.Variable(W0, name="W")
             tf.summary.histogram("weights", W)
             W = tf.tile(W, [8, 1])
-            x = tf.matmul(x, W)
+            return tf.matmul(x, W)
 
-            b = tf.Variable(tf.constant(0.0, shape=[f_out]), name="b")
-            tf.summary.histogram("bias", b)
-            x = x + b
+    if input_repr == 'invariant' and output_repr == 'regular':
+        if f_out is None:
+            f_out = f_in * 8
+        assert f_out % 8 == 0
 
-            if activation is not None:
-                x = activation(x)
-            return x
+        with tf.name_scope("{}-{}-8x{}".format(name, f_in, f_out // 8)):
+            W0 = tf.random_normal([f_in, f_out // 8])
+            W0 = W0 / math.sqrt(f_in)
+            W = tf.Variable(W0, name="W")
+            tf.summary.histogram("weights", W)
+            return tf.tile(tf.matmul(x, W), [1, 8])
 
-# pylint: disable=R0913, R0915
-def convolution(x, f_out=None, s=1, w=3,
-                input_repr='regular', output_repr='regular', padding='VALID',
-                activation=relu, name='conv'):
+    if input_repr == 'invariant' and output_repr == 'invariant':
+        if f_out is None:
+            f_out = f_in
+
+        with tf.name_scope("{}-{}-{}".format(name, f_in, f_out)):
+            W0 = tf.random_normal([f_in, f_out])
+            W0 = W0 / math.sqrt(f_in)
+            W = tf.Variable(W0, name="W")
+            tf.summary.histogram("weights", W)
+            return tf.matmul(x, W)
+
+# pylint: disable=R0913
+def convolution2(x_inv, x_reg, f_inv=None, f_reg=None, w=3, s=1,
+                 activation=relu, padding='VALID', name='convolution2'):
+    """take two tensors and output two tensors"""
+    if f_inv is None:
+        f_inv = x_inv.get_shape().as_list()[3]
+    if f_reg is None:
+        f_reg = x_reg.get_shape().as_list()[3] // 8
+
+    with tf.name_scope(name):
+        inv = conv2d(x_inv, f_inv, w, s, 'invariant', 'invariant', padding)
+        reg = conv2d(x_inv, 8*f_reg, w, s, 'invariant', 'regular', padding)
+
+        if x_reg is not None:
+            inv += conv2d(x_reg, f_inv, w, s, 'regular', 'invariant', padding)
+            reg += conv2d(x_reg, 8*f_reg, w, s, 'regular', 'regular', padding)
+            inv *= 1 / math.sqrt(2)
+            reg *= 1 / math.sqrt(2)
+
+        b = tf.Variable(tf.constant(0.0, shape=[f_inv]), name="b")
+        tf.summary.histogram("bias_inv", b)
+        inv += b
+
+        b = tf.Variable(tf.constant(0.0, shape=[f_reg]), name="b")
+        tf.summary.histogram("bias_reg", b)
+        reg += tf.tile(b, [8])
+
+        if activation:
+            inv = activation(inv)
+            reg = activation(reg)
+
+        return (inv, reg)
+
+def convolution(x, f_out=None, w=3, s=1,
+                activation=relu, input_repr='regular', output_repr='regular',
+                padding='VALID', name='convolution'):
+    """..."""
+    with tf.name_scope(name):
+        x = conv2d(x, f_out, s, w, input_repr, output_repr, padding)
+
+        if output_repr == 'invariant':
+            x += tf.Variable(tf.constant(0.0, shape=[f_out]), name="b")
+
+        if output_repr == 'regular':
+            x += tf.tile(tf.Variable(tf.constant(0.0, shape=[f_out // 8]), name="b"), [8])
+
+        return activation(x) if activation else x
+
+
+# pylint: disable=R0913, R0915, R0914, R0912
+def conv2d(x, f_out=None, w=3, s=1,
+           input_repr='regular', output_repr='regular', padding='VALID',
+           name='conv'):
     """The input and output tensor must tranform with the
      defining x (invariant / regular) representation
     where the x represent the tensor product between
@@ -160,16 +269,7 @@ def convolution(x, f_out=None, s=1, w=3,
                     for i in range(8)], 2)
                 for j in range(8)], 3)
 
-            # y = Conv(x, W)
-            x = tf.nn.conv2d(x, F, [1, s, s, 1], padding)
-
-            b = tf.Variable(tf.constant(0.0, shape=[f_out // 8]), name="b")
-            tf.summary.histogram("bias", b)
-            x = x + tf.tile(b, [8])
-
-            if activation is not None:
-                x = activation(x)
-            return x
+            return tf.nn.conv2d(x, F, [1, s, s, 1], padding)
 
     if input_repr == 'invariant' and output_repr == 'regular':
         if f_out is None:
@@ -178,15 +278,7 @@ def convolution(x, f_out=None, s=1, w=3,
 
         with tf.name_scope("{}-{}-8x{}".format(name, f_in, f_out // 8)):
             F = tf.concat(filters(f_in, f_out // 8), 3)
-            x = tf.nn.conv2d(x, F, [1, s, s, 1], padding)
-
-            b = tf.Variable(tf.constant(0.0, shape=[f_out // 8]), name="b")
-            tf.summary.histogram("bias", b)
-            x = x + tf.tile(b, [8])
-
-            if activation is not None:
-                x = activation(x)
-            return x
+            return tf.nn.conv2d(x, F, [1, s, s, 1], padding)
 
     if input_repr == 'regular' and output_repr == 'invariant':
         if f_out is None:
@@ -195,15 +287,51 @@ def convolution(x, f_out=None, s=1, w=3,
 
         with tf.name_scope("{}-8x{}-{}".format(name, f_in // 8, f_out)):
             F = tf.concat(filters(f_in // 8, f_out, n_mul=w * w * f_in), 2)
-            x = tf.nn.conv2d(x, F, [1, s, s, 1], padding)
+            return tf.nn.conv2d(x, F, [1, s, s, 1], padding)
 
-            b = tf.Variable(tf.constant(0.0, shape=[f_out]), name="b")
-            tf.summary.histogram("bias", b)
-            x = x + b
+    if input_repr == 'invariant' and output_repr == 'invariant':
+        if f_out is None:
+            f_out = f_in
 
-            if activation is not None:
-                x = activation(x)
-            return x
+        with tf.name_scope("{}-{}-{}".format(name, f_in, f_out)):
+            F0 = tf.random_normal([[0, 1, 1, 3, 3, 6, 0, 10][w], 1, 1, f_in, f_out])
+            F0 = F0 / math.sqrt(w * w * f_in)
+
+            F = tf.Variable(F0, name="F")
+            tf.summary.histogram("filter", F)
+
+            if w == 1:
+                p = [[0]]
+            elif w == 2:
+                p = [[0, 0],
+                     [0, 0]]
+            elif w == 3:
+                p = [[2, 1, 2],
+                     [1, 0, 1],
+                     [2, 1, 2]]
+            elif w == 4:
+                p = [[2, 1, 1, 2],
+                     [1, 0, 0, 1],
+                     [1, 0, 0, 1],
+                     [2, 1, 1, 2]]
+            elif w == 5:
+                p = [[5, 4, 3, 4, 5],
+                     [4, 2, 1, 2, 4],
+                     [3, 1, 0, 1, 3],
+                     [4, 2, 1, 2, 4],
+                     [5, 4, 3, 4, 5]]
+            elif w == 7:
+                p = [[9, 8, 7, 6, 7, 8, 9],
+                     [8, 5, 4, 3, 4, 5, 8],
+                     [7, 4, 2, 1, 2, 4, 7],
+                     [6, 3, 1, 0, 1, 3, 6],
+                     [7, 4, 2, 1, 2, 4, 7],
+                     [8, 5, 4, 3, 4, 5, 8],
+                     [9, 8, 7, 6, 7, 8, 9]]
+
+            F = tf.concat([tf.concat([F[p[j][i]] for i in range(w)], 1) for j in range(w)], 0)
+
+            return tf.nn.conv2d(x, F, [1, s, s, 1], padding)
 
 
 def max_pool(x, input_repr='regular'):
@@ -238,6 +366,8 @@ def concat(xs):
     return tf.concat(xs, f_dim)
 
 # pylint: disable=E1101
+
+
 def batch_normalization(x, acc, input_repr='regular', with_gamma=False):
     """Perform the amortized batch normalization, the mean and variance
     are accumulated according to the parameter acc.
@@ -260,8 +390,10 @@ def batch_normalization(x, acc, input_repr='regular', with_gamma=False):
         with tf.name_scope("bn-{}".format(f_in)):
             m, v = moments(x, axes=list(range(len(shape) - 1)))
 
-            acc_m = tf.Variable(tf.constant(0.0, shape=[f_in]), trainable=False, name="acc_m")
-            acc_v = tf.Variable(tf.constant(1.0, shape=[f_in]), trainable=False, name="acc_v")
+            acc_m = tf.Variable(tf.constant(
+                0.0, shape=[f_in]), trainable=False, name="acc_m")
+            acc_v = tf.Variable(tf.constant(
+                1.0, shape=[f_in]), trainable=False, name="acc_v")
             tf.summary.histogram("acc_m", acc_m)
             tf.summary.histogram("acc_v", acc_v)
 
@@ -283,8 +415,10 @@ def batch_normalization(x, acc, input_repr='regular', with_gamma=False):
             m = m / 8.0
             v = v / 8.0
 
-            acc_m = tf.Variable(tf.constant(0.0, shape=[f_in // 8]), trainable=False, name="acc_m")
-            acc_v = tf.Variable(tf.constant(1.0, shape=[f_in // 8]), trainable=False, name="acc_v")
+            acc_m = tf.Variable(tf.constant(
+                0.0, shape=[f_in // 8]), trainable=False, name="acc_m")
+            acc_v = tf.Variable(tf.constant(
+                1.0, shape=[f_in // 8]), trainable=False, name="acc_v")
             tf.summary.histogram("acc_m", acc_m)
             tf.summary.histogram("acc_v", acc_v)
 
@@ -296,9 +430,11 @@ def batch_normalization(x, acc, input_repr='regular', with_gamma=False):
             m.set_shape([f_in])
             v.set_shape([f_in])
 
-            beta = tf.tile(tf.Variable(tf.constant(0.0, shape=[f_in // 8])), [8])
+            beta = tf.tile(tf.Variable(
+                tf.constant(0.0, shape=[f_in // 8])), [8])
             tf.summary.histogram("beta", beta)
             if with_gamma:
-                gamma = tf.tile(tf.Variable(tf.constant(1.0, shape=[f_in // 8])), [8])
+                gamma = tf.tile(tf.Variable(
+                    tf.constant(1.0, shape=[f_in // 8])), [8])
                 tf.summary.histogram("gamma", gamma)
             return tf.nn.batch_normalization(x, m, v, beta, gamma if with_gamma else None, 1e-3)
